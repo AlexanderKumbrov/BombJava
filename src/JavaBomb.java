8faf67c2bc68a59300cdd53f65178ea4f2a5001f
alex <skainrusak@gmail.com>
2018-12-06 18:22:40 +0600

    Added choice number of bombs

--- src/JavaBomb.java
+++ src/JavaBomb.java
@@ -1,35 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import bomb.*;
 import bomb.Box;
 
-public class JavaBomb extends JFrame {
+public class JavaBomb extends JFrame  {
     private Game game ;            //Переменная класса Game
     private JPanel panel;         //Переменная класса JPanel Swing
+    private JPanel panel2;
     private final int COLS =9;   //Размер окна
     private final int ROWS = 9; //Размер окна
-    private final int BOMBS =5;  //Общее количество бомб
+    private int BOMBS ;                 //Общее количество бомб
     private final int IMAGE_SIZE=50;        //Размер картинок
     private JLabel label;
     private JLabel labelStat ;
     private int statLose = 0  ;
     private int statWin = 0;
-    private JPanel labelStatWin;
+    private JLabel lbl = new JLabel("Установите количество бомб");
+    private JRadioButton bombRadio1 = new JRadioButton("5 Бомб");
+    private JRadioButton bombRadio2 = new JRadioButton("8 Бомб");
+    private JRadioButton bombRadio3 = new JRadioButton("12 Бомб");
+    private JButton buttonStart = new JButton("Старт");
     public static void main(String[] args) {
         new JavaBomb();
 
     }
-    private JavaBomb(){
-        game = new Game(COLS , ROWS , BOMBS);  //Экземпляр Game в который
-        game.start();                         //передается размеры и количество бомб
-        setImages();
-        initLabelStat();
-        initLabel();                       //Вызов статуса
-        initPanel();
-        initFrame();
+
+    public JavaBomb(){
+        initPanel2();
+
     }
 
     private void initLabel(){
@@ -40,6 +42,17 @@ public class JavaBomb extends JFrame {
         labelStat = new JLabel("Stat");
         add(labelStat , BorderLayout.NORTH);
     }
+    private void initBombPanel(){
+
+
+        setImages();
+        initLabelStat();
+        initLabel();                       //Вызов статуса
+       initPanel();
+       initFrame();
+
+    }
+
 
     private void initPanel(){  //Создание панели с размером
         panel = new JPanel(){ //Рисование картинок
@@ -68,7 +81,6 @@ public class JavaBomb extends JFrame {
                 labelStat.setText(getMessageWin()+ getMessageLose()+statLose+"  You lose                            " +
                         "                                           You Win"+statWin );
 
-
             }
         });
 
@@ -77,6 +89,63 @@ public class JavaBomb extends JFrame {
                 Ranges.getSize().y*IMAGE_SIZE));        //Размер окна Dimension зависимость awt
         add(panel); //Добавление панели на форму
     }
+
+
+    ////////////////////////////////////////////////////////////////
+
+
+private void initPanel2(){
+        panel2 = new JPanel();
+panel2.setLayout(new GridLayout(5,5,2,2));
+
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setTitle("Java BOOOMB!");
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setResizable(false);  //запрет на изменение размера окна
+    setVisible(true);        //что бы видна форма
+    setIconImage(getImage("icon"));  //иконка программы
+    setLocationRelativeTo(null);  //Установка окна по центру
+    panel2.add(lbl);
+ButtonGroup group = new ButtonGroup();
+group.add(bombRadio1);
+group.add(bombRadio2);
+group.add(bombRadio3);
+
+panel2.add(bombRadio1);
+panel2.add(bombRadio2);
+panel2.add(bombRadio3);
+panel2.add(buttonStart);
+buttonStart.addActionListener(this::buttonStart);
+
+    bombRadio1.setSelected(true);
+    add(panel2);
+    pack();
+
+}
+public void buttonStart(ActionEvent e){
+        if (bombRadio1.isSelected()) {
+            BOMBS = 5;
+            game = new Game(COLS, ROWS, BOMBS);  //Экземпляр Game в который
+                                     //передается размеры и количество бомб
+            initBombPanel();
+            game.start();
+        }
+    if (bombRadio2.isSelected()) {
+        BOMBS = 8;
+        game = new Game(COLS, ROWS, BOMBS);  //Экземпляр Game в который
+        initBombPanel();                     //передается размеры и количество бомб
+        game.start();
+    }
+    if (bombRadio3.isSelected()){
+            BOMBS = 12;
+            game = new Game(COLS , ROWS , BOMBS);
+            initBombPanel();
+            game.start();
+    }
+
+panel2.setVisible(false);
+}
+/////////////////////////////////////////////////////////////////
 
     private String getMessage(){            //Сообщение при разных состояниях игры
         switch (game.getGameState()){
@@ -118,6 +187,9 @@ public class JavaBomb extends JFrame {
         pack();          // метод  устанавливает такой размер приложение что бы вместилось все
         setLocationRelativeTo(null);  //Установка окна по центру
      }
+
+
+
      private void setImages(){      //Загружаем картинки с enum
         for (Box box : Box.values()){
             box.image = getImage(box.name());
